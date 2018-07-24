ce19db53a5e3fb64299f7e728657971f6e918cc6
hotdev6m <hotdev_lm@outlook.com>
2018-07-24 23:17:08 +0530

    cleaned up console and comments

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@
 
 const server = require('./server');
 const port = process.env.PORT || 4001;
+const winston = require('./config/winston');
 
-server.appHttpServer.listen(port);
-
-/* TODO: Logger to be added
+//server.appHttpServer.listen(port);
 server.appHttpServer.listen(port, () => {
-  logger.info(`Drone server is running on port ${port}`);
+  winston.info(`Drone server is running on port ${port}`);
 });
-*/
+
 
 //////////////////  DRONE SIMULATOR ///////////////////////
 const drones = require('./droneParams');
--- src/simulator/drone.js
+++ src/simulator/drone.js
@@ -9,10 +9,7 @@ exports.simulate = (data) => {
   let port = data.port;
   let freq = data.freq;
 
-  //let server = http.createServer().listen(port);
-
   let server = http.createServer().listen(port, hostname, () => {
-    //console.log(`Drone# ${id} active at http://${hostname}:${port}/`)
     winston.info(`Drone# ${id} active at http://${hostname}:${port}/`);
   });
 
@@ -20,13 +17,11 @@ exports.simulate = (data) => {
   let io = socketIo(server);
   let interval = null;
   io.on('connection', socket => {
-    //console.log(`Drone# ${id} connected`);
     winston.info(`Drone# ${id} connected`);
     if(interval) {
       clearInterval(interval);
     }
     interval = setInterval(() => emitGeoLocation(socket, id), freq);
-    //socket.on('disconnect', () => console.log('Drone disconnected'));
     socket.on('disconnect', () => winston.info('Drone disconnected'));
   });
 };
@@ -35,7 +30,6 @@ const emitGeoLocation = (socket, id) => {
   try {
     socket.emit(`from_${id}`, randomData(id));
   } catch(error) {
-    //console.error(`Error: ${error} - Drone# ${id}`);
     winston.error(`Error: ${error} - Drone# ${id}`);
   }
 };
