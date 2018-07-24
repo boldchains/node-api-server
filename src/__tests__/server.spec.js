8615749e2dd6c9cbd2671e3c2db9947a76c648ce
hotdev6m <hotdev_lm@outlook.com>
2018-07-24 17:54:28 +0530

    readme file updated

--- src/__tests__/server.spec.js
+++ src/__tests__/server.spec.js
@@ -1,9 +1,14 @@
 import assert from 'assert';
+const request = require('supertest');
+const express = require('express');
+const app = express();
 
-describe('Test - Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World' })
+});
+
+describe('Test the root path', () => {
+  test('It should response the GET method', () => {
+    return request(app).get('/').expect(200)
   });
 });
