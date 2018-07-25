ff0b393b629cdf17df0bbfa47ed8a307577b30f4
hotdev6m <hotdev_lm@outlook.com>
2018-07-25 11:57:13 +0530

    removed code on winston error handling part for now

--- src/server.js
+++ src/server.js
@@ -23,19 +23,6 @@ const app = express();
 
 // app middleware
 app.use(morgan('combined', {stream: winston.stream }));
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // add winston logging
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 app.use(cors());
 app.use(bodyParser.json({type: 'application/json'}));
 app.disable('etag');
